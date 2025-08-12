f7360643c9b2c6e10cdbd1d52700a5dff59aa2f6
Prohor722 <prohor.b722@gmail.com>
2025-08-12 23:38:52 +0600

    basic shadcn table added

--- app/companions/CompanionList.tsx
+++ app/companions/CompanionList.tsx
@@ -1,8 +1,26 @@
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
 import React from 'react'
 
 const CompanionList = () => {
   return (
-    <div>CompanionList</div>
+    <article>
+        <h2>Recent Sessions</h2>
+
+        <Table>
+            <TableCaption></TableCaption>
+            <TableHeader>
+                <TableRow>
+                    <TableHead></TableHead>
+                </TableRow>
+            </TableHeader>
+
+            <TableBody>
+                <TableRow>
+                    <TableCell></TableCell>
+                </TableRow>
+            </TableBody>
+        </Table>
+    </article>
   )
 }
 
