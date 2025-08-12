011120857d8806a203bf5ceb5d0dbf0c41bde990
Prohor722 <prohor.b722@gmail.com>
2025-08-12 23:28:07 +0600

    companion cards ui done

--- app/companions/CompanionCard.tsx
+++ app/companions/CompanionCard.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 interface CompanionCardType {
   id: string;
@@ -16,7 +18,29 @@ const CompanionCard = ({
   duration,
   color,
 }: CompanionCardType) => {
-  return <div>CompanionCard</div>;
+  return <article className="companion-card" style={{ backgroundColor: color}}>
+    <div className="flex justify-between items-center ">
+        <div className="subject-badge">{subject}</div>
+        <button className="companion-bookmark">
+            <Image src="/icons/bookmark.svg" alt="bookmark"
+                height={15} width={12.5} />
+        </button>
+    </div>
+
+    <h2 className="text-2xl font-bold">{name}</h2>
+    <p className="text-sm">{topic}</p>
+
+    <div className="flex items-center gap-2">
+        <Image src="/icons/clock.svg" width={13.5} height={13.5}
+            alt="clock" />
+        <p className="text-sm">{duration} mins</p>
+    </div>
+    <Link href={`/companions/${id}`} className="w-full">
+        <button className="btn-primary w-full justify-center">
+            Lunch Lession
+        </button>
+    </Link>
+  </article>;
 };
 
 export default CompanionCard;
